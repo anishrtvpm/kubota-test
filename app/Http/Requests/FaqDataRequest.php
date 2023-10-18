7f41e11ae8c957d0a6d82af5cec3cb7b7a1bfe93
deepthy-a <deepthy.a@experionglobal.com>
2023-10-18 18:28:09 +0900

    Image upload moved to public folder

--- app/Http/Requests/FaqDataRequest.php
+++ app/Http/Requests/FaqDataRequest.php
@@ -32,19 +32,20 @@ class FaqDataRequest extends FormRequest
             'responder' => ['required', 'string', 'max:100'],
             'status' => ['required', 'integer'],
             'language' => ['required', 'string'],
-            // 'a_message' => ['required'],
-            //  'q_message' => ['required'],
-            'files.*' => [
+            'a_message' => ['required'],
+            'q_message' => ['required'],
+            'display_group' => 'required',
+          /*  'files.*' => [
                 'file',
                 'mimes:gif,tif,png,jpg,mov,mpg,wma,pdf,doc,docx,xls,xlsx,ppt,pptx,txt,csv,zip',
                 function ($attribute, $value, $fail) {
                     $maxSize = $this->hasVideoExtension($value) ? 500 * 1024 : 100 * 1024;
 
                     if ($value->getSize() > $maxSize) {
-                        $fail("The $attribute must not exceed " . ($maxSize / 1024) . " KB.");
+                        $fail("The $attribute must not exceed " . ($maxSize / 1024) . " MB.");
                     }
                 },
-            ],
+            ], */
         ];
     }
 
@@ -58,6 +59,8 @@ class FaqDataRequest extends FormRequest
             'sort.numeric' => '表示順は半角数値で入力してください。',
             'title.required' => 'タイトルは必須項目です。',
             'title.max' => 'タイトルは 100文字以内で設定してください。',
+            'q_message.required' => '質問内容は必須項目です。',
+            'a_message.required' => '回答内容は必須項目です。',
             'reference_url.max' => '参加URLは 8000文字以内で設定してください。',
             'question_date.required' => '質問日は必須項目です。',
             'answer_date.required' => '回答日は必須項目です。',
@@ -65,10 +68,11 @@ class FaqDataRequest extends FormRequest
             'responder.max' => '回答者は 100文字以内で設定してください。',
             'status.required' => '状態は必須項目です。',
             'language.required' => '言語は必須項目です。',
+            'display_group.required' => '配信先は必須項目です。',
             'answer_date.after_or_equal' => '回答日は質問日と同じかそれ以上でなければなりません。',
             'files.array' => '3つのファイルのみアップロードしてください。',
-            'files.*.file' => 'Invalid file.',
-            'files.*.mimes' => 'Unsupported file format.',
+            'files.*.file' => 'ファイルが無効です。',
+            'files.*.mimes' => '無効なファイル拡張子です。',
         ];
     }
 
